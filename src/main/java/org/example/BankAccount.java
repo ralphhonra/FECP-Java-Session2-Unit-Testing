7503a14c14855bcf95386fdeab4d4e9ac6d41e66
Ralph Daniel Honra <ralphdaniel.honra@gcash.com>
2025-07-03 15:12:03 +0800

    v1.0 added bank account encapsulation

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class BankAccount {
+    private int accountNumber;
+    private String bankHolderName;
+    private int availableBalance;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getBankHolderName() {
+        return bankHolderName;
+    }
+
+    public void setBankHolderName(String bankHolderName) {
+        this.bankHolderName = bankHolderName;
+    }
+
+    public int getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public void setAvailableBalance(int availableBalance) {
+        this.availableBalance = availableBalance;
+    }
+}
